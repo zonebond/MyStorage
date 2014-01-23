8840ccfdd36a4626e2f5ab2801281a922c9e43d8
zonebond <zonebond@126.com>
2014-01-23 17:27:30 +0800

    tomcat command
    Command tool of tomcat for windows

---
+++ tomcat.bat
@@ -0,0 +1,69 @@
+@echo off
+
+set "CURRENT_DIR=%cd%"
+
+set cmd=%1
+if "%cmd%"=="" goto show_welcome
+if not exist "C:\Zonebond Garden\WebContainer\apache-tomcat-6.0.37\bin" goto error_not_exist
+cd "C:\Zonebond Garden\WebContainer\apache-tomcat-6.0.37\bin\"
+
+if %cmd%==start goto start
+if %cmd%==stop goto stop
+if %cmd%==restart goto restart
+
+if %cmd%==conf goto conf_dir
+if %cmd%==version goto version
+
+goto end
+
+
+:show_welcome
+echo ------ "Welcome to Tomcat Base" ------
+ echo.
+ echo.
+echo commands: start / stop / restart
+ echo.
+echo exp:
+ echo.
+echo -------------
+ echo.
+echo "tomcat [command]"
+ echo.
+set "tomcat_bin = abc"
+goto end
+
+:start
+call startup.bat
+goto end
+
+:stop
+call shutdown.bat
+goto end
+
+:restart
+call shutdown.bat && call startup.bat
+goto end
+
+:version
+call version.bat
+pause
+goto end
+
+:conf_dir
+set file_name=%2
+if "%file_name%"=="" goto error_conf
+cd ../conf
+if not exist %file_name%.xml goto error_not_exist
+start notepad %file_name%.xml && goto end
+exit
+
+:error_conf
+echo Please enter a configuration file name!
+goto end
+
+:error_not_exist
+echo The target [Directory] or [File] is not exist!
+goto end
+
+:end
+cd "%CURRENT_DIR%"
